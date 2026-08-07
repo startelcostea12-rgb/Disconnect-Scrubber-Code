e167532a4e8586fc8a3c73f0bba54bd0d8dd0310
Startel Costea <startel.costea12@gmail.com>
2026-08-07 10:35:35 -0400

    Fix API endpoint URL and restore filtering

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file, flash, redirect, url_for
+﻿from flask import Flask, render_template, request, send_file, flash, redirect, url_for
 import pandas as pd
 import requests
 import re
